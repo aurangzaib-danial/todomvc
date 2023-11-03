1f3ce652a2345a97f484bf1a3b8ddc0d024bbcf2
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2023-11-03 20:56:08 +0000

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(e) {
+    setNewTodo(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            value={newTodo}
+            autoFocus
+            onChange={handleChange} />
+        </header>
+        <section className="main">
+          <input
+            id="toggle-all"
+            className="toggle-all"
+            type="checkbox" />
+          <label for="toggle-all">
+            Mark all as complete
+          </label>
+          <ul className="todo-list">
+            <li class="completed">
+              <div className="view">
+                <input
+                  className="toggle"
+                  type="checkbox" />
+                <label>Buy milk</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <footer className="footer">
+          <span className="todo-count"><strong>1</strong> item left</span>
+          <ul className="filters">
+            <li>
+              <a href="#/" className="selected">All</a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button class="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>Create by Aurangzaib Khan</p>
+      </footer>
+    </>
   );
 }
 
